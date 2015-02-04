d56df3e25e51288d92842139994ff85774240202
Kerde Severin <kerde.severin@greyhound.com>
2015-02-04 15:49:17 -0600

    Initial Commit

---
+++ WatchKit_SimpleTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKit_SimpleTable
+//
+//  Created by Kerde Severin on 2/4/15.
+//  Copyright (c) 2015 Kerde Severin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
